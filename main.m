50f3e02a3470eac9505a885df8d1f4f239866aa0
Ant89ne <antoine.bralet@cpe.fr>
2020-10-16 10:54:34 +0200

    Recalage grossier OK

--- main.m
+++ main.m
@@ -5,12 +5,17 @@ close all;
 
 load("P_point.mat");
 load("Q_point.mat");
+load("set0.xyz");
+load("set1.xyz");
 
 [T,R] = Recalage2(P_point,Q_point)
 
+set0(:,1:3) = (R*set0(:,1:3)')' + T';
 
+P = set0(:,1:3);
+Q = set1(:,1:3);
 
 %dt = DelaunayTri (A) ;
-%[ pid d ] = near e s tNe ighbor ( dt ,B) ;
+%[ pid d ] = nearestNeighbor( dt ,B) ;
 
 
