a479b11000a8c9b64fdd1bbbf57a8741531af4a1
Guillaume <guillaumeduret1@gmail.com>
2020-10-16 09:37:27 +0200

    R et T

---
+++ Recalage2.m
@@ -0,0 +1,37 @@
+
+clear variables;
+close all;
+
+n=10;
+P_point = zeros (n,3);
+Q_point = zeros (n,3);
+
+
+mean(P_point)
+moy_P = mean(P_point);
+moy_Q = mean(Q_point);
+P_point_centre = P_point - moy_P;
+Q_point_centre = Q_point - moy_Q;
+
+
+M = P_point_centre' * Q_point_centre
+
+[ U,S,V ] = svd (M);
+
+R_diag = eye(3,3);
+R_diag(3,3)=det (V*U') ;
+R = V * R_diag * U'
+
+
+T = moy_Q' - R * moy_P'
+
+
+
+
+
+
+
+
+
+
+
