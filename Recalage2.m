0bfd8aee8c54e712eef7737ee65d4c1841dbf7ad
Guillaume <guillaumeduret1@gmail.com>
2020-10-16 10:13:02 +0200

    function

--- Recalage2.m
+++ Recalage2.m
@@ -1,27 +1,29 @@
 
-clear variables;
-close all;
-
-load("P_point.mat");
-load("Q_point.mat");
-
-mean(P_point)
-moy_P = mean(P_point);
-moy_Q = mean(Q_point);
-P_point_centre = P_point - moy_P;
-Q_point_centre = Q_point - moy_Q;
+function T,R = Recalage2(P_point, Q_point)
 
 
-M = P_point_centre' * Q_point_centre
 
-[ U,S,V ] = svd (M);
+    %load("P_point.mat");
+    %load("Q_point.mat");
 
-R_diag = eye(3,3);
-R_diag(3,3)=det (V*U') ;
-R = V * R_diag * U'
+    moy_P = mean(P_point);
+    moy_Q = mean(Q_point);
+    P_point_centre = P_point - moy_P;
+    Q_point_centre = Q_point - moy_Q;
 
 
-T = moy_Q' - R * moy_P'
+    M = P_point_centre' * Q_point_centre
+
+    [ U,S,V ] = svd (M);
+
+    R_diag = eye(3,3);
+    R_diag(3,3)=det (V*U') ;
+    R = V * R_diag * U';
+
+
+    T = moy_Q' - R * moy_P';
+
+end
 
 
 
